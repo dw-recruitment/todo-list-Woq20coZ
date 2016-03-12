181fdac756f9a6c95539c3f1c361a00680d9b571
Anonymous Candidate <anon@example.com>
2016-03-12 15:56:53 -0800

    Complete Step 1

---
+++ todo-list/src/todo_list/core.clj
@@ -0,0 +1,13 @@
+(ns todo-list.core
+  (:gen-class))
+
+(defn hello [] 
+	(println "Hello world!"))
+
+(defn -main [] 
+	(hello))
+
+; (defn -main
+;   "I don't do a whole lot ... yet."
+;   [& args]
+;   (println "Hello, World!"))
