13cded72c84c26a3fd47b6ea5458fcdf79337895
Anonymous Candidate <anon@example.com>
2016-03-12 16:17:42 -0800

    added subfolders to resources

--- todo-list/project.clj
+++ todo-list/project.clj
@@ -1,5 +1,5 @@
 (defproject todo-list "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "todo-list: a list of todo items"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
