8b1bc82608b81ff5fb8698e23e9fbb3d4e73e75f
Anonymous Candidate <anon@example.com>
2016-03-14 13:28:14 -0700

    trying to get under construction giphy to appear

--- todo-list/src/todo_list/core.clj
+++ todo-list/src/todo_list/core.clj
@@ -1,11 +1,18 @@
-(ns todo-list.core
-  (:gen-class))
+; (ns todo-list.core
+;   (:gen-class))
+(ns example.core
+	(:require
+		[ring.adapter.jetty :as jetty]))
 
 (defn hello [] 
-	(println "Hello world!"))
+	(println "http://giphy.com/gifs/please-construction-patient-12onZb4hOx3xza"))
 
-(defn -main [] 
-	(hello))
+(defn -main []
+	(jetty/run-jetty
+		(fn [req] {:status 200 :body "http://giphy.com/gifs/please-construction-patient-12onZb4hOx3xza"})
+		{:port 5000}))
+; (defn -main [] 
+	; (hello))
 
 ; (defn -main
 ;   "I don't do a whole lot ... yet."
